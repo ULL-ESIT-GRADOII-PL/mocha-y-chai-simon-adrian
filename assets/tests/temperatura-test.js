81bb89daec2f08588e0c74f1aaa31517bbddc0bf
Simón Fuertes Sánchez <simonfuesan@gmail.com>
2016-03-16 22:34:21 +0000

    Acabados test Temperatura y subclases

--- assets/tests/temperatura-test.js
+++ assets/tests/temperatura-test.js
@@ -22,60 +22,110 @@ describe("Temperatura", function() {
       expect(herencia).to.be.instanceof(Medida);
     });
   });
-/*
-  describe("#toPulgadas", function() {
-    it("Debería convertir Metros a Pulgadas", function() {
-      var metrosAPulgadas = new Temperatura("23m");
-      var pulgadas = metrosAPulgadas.toPulgadas();
-      expect(pulgadas).to.be.instanceof(Pulgadas);
-      expect(pulgadas.getValue()).to.equal(905.51);
-      expect(pulgadas.getType()).to.equal('Pulgadas');
+
+  describe("#toCelsius", function() {
+    it("Debería convertir Kelvin a Celsius", function() {
+      var kelvinACelsius = new Temperatura("23k");
+      var celsius = kelvinACelsius.toCelsius();
+      expect(celsius).to.be.instanceof(Celsius);
+      expect(celsius.getValue()).to.equal(-250.14999999999998);
+      expect(celsius.getType()).to.equal('Celsius');
+    });
+
+    it("Debería convertir Farenheit a Celsius", function() {
+      var farenACelsius = new Temperatura("23f");
+      var celsius = farenACelsius.toCelsius();
+      expect(celsius).to.be.instanceof(Celsius);
+      expect(celsius.getValue()).to.equal(-5);
+      expect(celsius.getType()).to.equal('Celsius');
     });
   });
 
-  describe("#toMetros", function() {
-    it("Debería convertir Pulgadas a Metros", function() {
-      var pulgadasAMetros = new Temperatura("23p");
-      var metros = pulgadasAMetros.toMetros();
-      expect(metros).to.be.instanceof(Metros);
-      expect(metros.getValue()).to.equal(0.5842011684023368);
-      expect(metros.getType()).to.equal('Metros');
+  describe("#toFahrenheit", function() {
+    it("Debería convertir Celsius a Farenheit", function() {
+      var celsiusAFaren = new Temperatura("23c");
+      var faren = celsiusAFaren.toFahrenheit();
+      expect(faren).to.be.instanceof(Fahrenheit);
+      expect(faren.getValue()).to.equal(73.4);
+      expect(faren.getType()).to.equal('Fahrenheit');
+    });
+
+    it("Debería convertir Kelvin a Fahrenheit", function() {
+      var kelvinAFaren = new Temperatura("23k");
+      var faren = kelvinAFaren.toFahrenheit();
+      expect(faren).to.be.instanceof(Fahrenheit);
+      expect(faren.getValue()).to.equal(-418.27000000000004);
+      expect(faren.getType()).to.equal('Fahrenheit');
     });
   });
-*/
+
+  describe("#toKelvin", function() {
+    it("Debería convertir Celsius a Kelvin", function() {
+      var celsiusAKelvin = new Temperatura("23c");
+      var kelvin = celsiusAKelvin.toKelvin();
+      expect(kelvin).to.be.instanceof(Kelvin);
+      expect(kelvin.getValue()).to.equal(296.15);
+      expect(kelvin.getType()).to.equal('Kelvin');
+    });
+
+    it("Debería convertir Fahrenheit a Kelvin", function() {
+      var farenAKelvin = new Temperatura("23f");
+      var kelvin = farenAKelvin.toKelvin();
+      expect(kelvin).to.be.instanceof(Kelvin);
+      expect(kelvin.getValue()).to.equal(268.15);
+      expect(kelvin.getType()).to.equal('Kelvin');
+    });
+  });
+
 });
-/*
-describe("Metros", function() {
+
+describe("Celsius", function() {
   describe("Constructor", function() {
-    it("Debe aceptar un valor y guardarlo con tipo 'Metros'", function() {
-      var metros = new Metros(25);
-      expect(metros.getValue()).to.equal(25);
-      expect(metros.getType()).to.equal('Metros');
+    it("Debe aceptar un valor y guardarlo con tipo 'Celsius'", function() {
+      var temperatura = new Celsius(25);
+      expect(temperatura.getValue()).to.equal(25);
+      expect(temperatura.getType()).to.equal('Celsius');
     });
   });
 
   describe("Herencia", function() {
-    it("Una instancia de 'Metros' debe ser hija de la clase Longitud", function() {
-      var metros = new Metros(25);
-      expect(metros).to.be.instanceof(Temperatura);
+    it("Una instancia de 'Celsius' debe ser hija de la clase Temperatura", function() {
+      var temperatura = new Celsius(25);
+      expect(temperatura).to.be.instanceof(Temperatura);
     });
   });
 });
 
-describe("Pulgada", function() {
+describe("Fahrenheit", function() {
   describe("Constructor", function() {
-    it("Debe aceptar un valor y guardarlo con tipo 'Pulgadas'", function() {
-      var pulgadas = new Pulgadas(25);
-      expect(pulgadas.getValue()).to.equal(25);
-      expect(pulgadas.getType()).to.equal('Pulgadas');
+    it("Debe aceptar un valor y guardarlo con tipo 'Fahrenheit'", function() {
+      var temperatura = new Fahrenheit(25);
+      expect(temperatura.getValue()).to.equal(25);
+      expect(temperatura.getType()).to.equal('Fahrenheit');
     });
   });
 
   describe("Herencia", function() {
-    it("Una instancia de 'Metros' debe ser hija de la clase Longitud", function() {
-      var pulgadas = new Pulgadas(25);
-      expect(pulgadas).to.be.instanceof(Temperatura);
+    it("Una instancia de 'Fahrenheit' debe ser hija de la clase Temperatura", function() {
+      var temperatura = new Fahrenheit(25);
+      expect(temperatura).to.be.instanceof(Temperatura);
     });
   });
 });
-*/
+
+describe("Kelvin", function() {
+  describe("Constructor", function() {
+    it("Debe aceptar un valor y guardarlo con tipo 'Kelvin'", function() {
+      var temperatura = new Kelvin(25);
+      expect(temperatura.getValue()).to.equal(25);
+      expect(temperatura.getType()).to.equal('Kelvin');
+    });
+  });
+
+  describe("Herencia", function() {
+    it("Una instancia de 'Kelvin' debe ser hija de la clase Temperatura", function() {
+      var temperatura = new Kelvin(25);
+      expect(temperatura).to.be.instanceof(Temperatura);
+    });
+  });
+});
